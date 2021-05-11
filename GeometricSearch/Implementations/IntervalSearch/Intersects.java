62f9d514f2d458428ecec4f925afcbb9f848e674
Arjun Subramanian <arjun.subramanian.08@gmail.com>
2021-05-11 10:17:11 -0400

    new orgainization

--- GeometricSearch/Implementations/IntervalSearch/Intersects.java
+++
@@ -1,5 +0,0 @@
-package GeometricSearch.Implementations.IntervalSearch;
-
-public interface Intersects<Key> {
-    public boolean intersects(Key lo, Key hi);
-}
--- UndirectedGraphs/TeachingCode/Paths/Paths.java
+++
@@ -1,9 +0,0 @@
-package UndirectedGraphs.TeachingCode.Paths;
-
-public interface Paths {
-    // Paths(Graph G, int s); //find paths in G from source s
-
-    boolean hasPathTo(int v); // is there a path from s to v?
-
-    Iterable<Integer> pathTo(int v); // path from s to v; null if no such path
-}
